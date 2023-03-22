204c4cb46b9eae73256024fc994fd7a3da7bf015
sghosh-adastra <sourish.ghosh@adastragrp.com>
2023-03-22 16:00:35 +0100

    tf-ekscluster

---
+++ src/terraform/igw.tf
@@ -0,0 +1,9 @@
+#Internet Gateway
+
+resource "aws_internet_gateway" "igw" {
+  vpc_id = aws_vpc.main.id
+
+  tags = {
+    Name = "igw"
+  }
+}
---
+++ src/terraform/oidc.tf
@@ -0,0 +1,11 @@
+#Manage permissions for your application to be deployed on K8s
+
+data "tls_certificate" "eks" {
+  url = aws_eks_cluster.demo.identity[0].oidc[0].issuer
+}
+
+resource "aws_iam_openid_connect_provider" "eks" {
+  client_id_list  = ["sts.amazonaws.com"]
+  thumbprint_list = [data.tls_certificate.eks.certificates[0].sha1_fingerprint]
+  url             = aws_eks_cluster.demo.identity[0].oidc[0].issuer
+}
